34cd863ac100553e695aee3aa20693ddde6001c1
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2023-08-19 17:26:08 +0200

    Debut intégration dark mode

--- js/toggle-dark.js
+++ js/toggle-dark.js
@@ -1,6 +1,20 @@
 const toggle = document.getElementById("toggle");
 const body = document.querySelector("body");
+const section = document.querySelector("section");
+const navToggler = document.querySelector(".nav-toggler");
+const navTogglerSpan = document.querySelector(".nav-toggler span");
+const navInner = document.querySelector(".nav-inner");
+const headerLia = document.querySelector(".header li a");
+const headerLiaBefor = document.querySelector(".header li a::before");
+
+
+
+
 toggle.onclick = function () {
     toggle.classList.toggle("dark");
     body.classList.toggle("dark");
+    section.classList.toggle("dark");
+    navToggler.classList.toggle("dark");
+    navTogglerSpan.classList.toggle("dark");
+    navInner.classList.toggle("dark");
 };
