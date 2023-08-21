dc412cc20d8b0fded3bd79556b9fc7ef3da872bd
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2023-08-21 16:00:48 +0200

    Edit dark mode home

--- js/toggle-dark.js
+++ js/toggle-dark.js
@@ -1,11 +1,13 @@
 const toggle = document.getElementById("toggle");
 const body = document.querySelector("body");
 const section = document.querySelector("section");
+const homImg = document.querySelector(".home-img .img-box img");
 const navToggler = document.querySelector(".nav-toggler");
 const navTogglerSpan = document.querySelector(".nav-toggler span");
 const navInner = document.querySelector(".nav-inner");
-const headerLia = document.querySelector(".header li a");
-const headerLiaBefor = document.querySelector(".header li a::before");
+const navInnerUl = document.querySelectorAll(".nav-inner ul li");
+const navInnerA = document.querySelectorAll(".nav-inner ul li a");
+const btn = document.querySelectorAll(".btn");
 
 
 
@@ -14,7 +16,11 @@ toggle.onclick = function () {
     toggle.classList.toggle("dark");
     body.classList.toggle("dark");
     section.classList.toggle("dark");
+    homImg.classList.toggle("dark");
     navToggler.classList.toggle("dark");
     navTogglerSpan.classList.toggle("dark");
     navInner.classList.toggle("dark");
+    navInnerUl.forEach(link => link.classList.toggle("dark"));
+    navInnerA.forEach(link => link.classList.toggle("dark"));
+    btn.forEach(btn => btn.classList.toggle("dark"));
 };
