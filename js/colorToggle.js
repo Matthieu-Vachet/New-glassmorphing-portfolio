a8e505430ac28aad16b8a154b2a3e5a546e59521
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2023-08-28 19:03:47 +0200

    Edit

--- js/colorToggle.js
+++ js/colorToggle.js
@@ -1,4 +1,5 @@
 const colorToggle = document.querySelector(".colorToggle");
+const bodyElement = document.querySelector("body");
 
 // Fonction pour fermer le menu
 function closeMenu() {
@@ -18,3 +19,15 @@ document.addEventListener("click", (event) => {
     closeMenu();
   }
 });
+
+// Écouter l'événement click sur les couleurs du menu
+const colorItems = document.querySelectorAll(".menu-color li");
+colorItems.forEach((colorItem) => {
+  colorItem.addEventListener("click", (event) => {
+    const colorClass = event.target.classList[2]; // Obtenir la classe de couleur (par exemple, "orange", "red", etc.)
+    bodyElement.className = ""; // Réinitialiser toutes les classes existantes sur l'élément body
+    bodyElement.classList.add(colorClass); // Ajouter la nouvelle classe de couleur à l'élément body
+    closeMenu();
+  });
+});
+
